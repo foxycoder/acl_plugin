3a2320bb591c14d40284d57aa16ed13529f6f14c
Wouter R. de Vos <wouter.devos@surecreations.com>
2010-08-24 15:53:28 +0200

    made compatible with our apps

--- acl_app_controller.php
+++ acl_app_controller.php
@@ -1,6 +1,20 @@
 <?php
 
 class AclAppController extends AppController {
+	
+	/**
+	 * Controller beforeFilter callback.
+	 * Called before the controller action. 
+	 * 
+	 * @return void
+	 */
+	function beforeRender() {
+		$this->view = 'View';
+		if (!$this->RequestHandler->isAjax())
+			$this->layout = 'default';
+	}
+	
+	
 	function success() {
 		header("HTTP/1.0 200 Success", null, 200);
 		exit;
